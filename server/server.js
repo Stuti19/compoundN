6012f0017d3e309c52cb810c03d3cdcd12bea6fa
Stuti19 <stutib19@gmail.com>
2025-03-13 00:25:28 +0530

    Update MongoDB connection configuration

--- server/server.js
+++ server/server.js
@@ -8,13 +8,23 @@ const { sendotp } = require('./controllers/otpController');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',  // Allow all origins in development
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/compoundN')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect('mongodb+srv://compoundforever01:2workHARD%40compoundn%40@cluster0.enn9s.mongodb.net/compoundN?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Create nodemailer transporter
 const transporter = nodemailer.createTransport({
